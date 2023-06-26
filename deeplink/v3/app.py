4d04bc811022fd856663fbec234829dcd5d165bc
Alvin Wan <alvinwan1@gmail.com>
2023-06-26 03:40:47 -0700

    added more generic deeplink support

---
+++ deeplink/v3/app.py
@@ -0,0 +1,14 @@
+from PyQt6.QtWidgets import QApplication
+from PyQt6.QtCore import QEvent
+
+
+class MyApp(QApplication): # subclass application
+    def event(self, event: QEvent): # custom event handler
+        if event.type() == QEvent.Type.FileOpen: # handle "File Open" event
+            print(event.url())  # log the deeplink to console
+        return super().event(event)
+
+
+if __name__ == "__main__":
+    app = MyApp([])
+    app.exec()
---
+++ deeplink/v3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+OPTIONS = {  # notice argv_emulation has been removed
+    'plist': {
+        'CFBundleURLTypes': [  # for custom URL schemes (i.e., deeplinks)
+            {
+                'CFBundleURLName': 'MyApplication',  # arbitrary
+                'CFBundleURLSchemes': ['myapp'],  # deeplink will be myapp://
+            },
+        ],
+    },
+}
+
+setup(
+    app=['app.py'],
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app', 'pyqt6'],  # add other dependencies here
+)
