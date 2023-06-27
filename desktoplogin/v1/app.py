2ab4230b5ac9ef278252bd576075226cdb19ddc3
Alvin Wan <alvinwan1@gmail.com>
2023-06-27 03:23:55 -0700

    add SSL

--- desktoplogin/v1/app.py
+++ desktoplogin/v1/app.py
@@ -1,16 +1,16 @@
 from flask import Flask, session
-from flask_google_login import FlaskGoogleLogin
+from simple_flask_google_login import SimpleFlaskGoogleLogin
 import webview
 
 
 app = Flask("Google Login App")
 app.secret_key = "YourSecretKeyHere"  # Secret key is needed for OAuth 2.0
-login = FlaskGoogleLogin(app)
+login = SimpleFlaskGoogleLogin(app)
 
 
 @app.route("/")
 def index():
-    if 'google_id' in session:
+    if 'name' in session:
         return f"Hello {session['name']}! <a href='/logout'>Logout</a>"
     return "<a href='/login'>Login</a>"
 
