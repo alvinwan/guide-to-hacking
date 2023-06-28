5261f3ea7814ff1806256c3205d042a01aa4f416
Alvin Wan <alvinwan1@gmail.com>
2023-06-28 02:17:37 -0700

    updated reqs for desktop app

--- desktopapp/v5-webview/app.py
+++ desktopapp/v5-webview/app.py
@@ -1,18 +1,20 @@
+import sys
 import webview
-import webview.platforms.qt
 from qtpy.QtWidgets import QApplication
 from qtpy.QtCore import QEvent
+import webview.platforms.qt  # needed for us to define a custom application
 
 
-def event_handler(self, event: QEvent):
-    if event.type() == QEvent.Type.ApplicationActivate:
-        window.set_title("In focus")
-    if event.type() == QEvent.Type.ApplicationDeactivate:
-        window.set_title("Not in focus")
-    return super(QApplication, self).event(event)
-webview.platforms.qt.QApplication.event = event_handler  # HACK: monkey patch webview's qt event handler
+class MainApplication(QApplication):
+    def event(self, event: QEvent):
+        if event.type() == QEvent.Type.ApplicationActivate:
+            window.set_title("In focus")
+        if event.type() == QEvent.Type.ApplicationDeactivate:
+            window.set_title("Not in focus")
+        return super(QApplication, self).event(event)
 
 
 if __name__ == '__main__':
+    app = MainApplication(sys.argv)
     window = webview.create_window('Hello world', 'https://alvinwan.com/blog/')
     webview.start(gui='qt')
