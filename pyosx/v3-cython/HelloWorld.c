efe7044d33da1ce6075a82e7a320a7ab17a17bd3
Alvin Wan <alvinwan1@gmail.com>
2023-10-22 21:19:07 -0700

    rename to cython

--- pyosx/v3-cython/HelloWorld.c
+++ pyosx/v3-cython/HelloWorld.c
@@ -5,9 +5,17 @@
 static PyObject* sayHello(PyObject* self, PyObject* args) {
     // Load the Objective-C dynamic library
     void* handle = dlopen("HelloWorld.dylib", RTLD_LAZY);
+    if (!handle) {
+        printf("HelloWorld.dylib shared library was not found.");
+        return NULL;
+    }
     
     // Load and call the function
     void (*func)() = dlsym(handle, "sayHello");
+    if (!func) {
+        printf("sayHello function was not found.");
+        return NULL;
+    }
     func();
     
     // Cleanup
@@ -16,14 +24,14 @@ static PyObject* sayHello(PyObject* self, PyObject* args) {
 }
 
 static PyMethodDef methods[] = {
-    {"sayHello", sayHello, METH_NOARGS, "Call Objective-C sayHello."},
+    {"sayHello", sayHello, METH_NOARGS, "Says 'Hello, World!'"},
 };
 
 static struct PyModuleDef HelloWorld_module = {
     PyModuleDef_HEAD_INIT,
-    "HelloWorld",  // Name of the module
-    NULL,     // Documentation string
-    -1,       // Size of per-interpreter state of the module
+    "HelloWorld",   // Name of the module
+    NULL,           // Documentation string
+    -1,             // Size of per-interpreter state of the module
     methods
 };
 
